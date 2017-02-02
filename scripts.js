2e06a1d9c64af8ab158bcb2e170f70a84814a3fe
Kelly Miller <kellyannemiller@gmail.com>
2017-02-02 08:24:42 -0700

    Potential final for eval

--- scripts.js
+++ scripts.js
@@ -7,47 +7,44 @@ var showLastGuess = document.getElementById('show-user-guess')
 var secretNumber = resetFunction();
 
 //EventListeners
+userInput.addEventListener('keyup', function(){
+  clearButton.disabled = false;
+})
+
 clearButton.addEventListener('click', function() {
   userInput.value = null;
+  clearButton.disabled = true;
+});
+
+resetButton.addEventListener('click', function () {
+  secretNumber  = resetFunction();
   clearButton.disabled = true;
   resetButton.disabled = true;
 });
 
-userInput.addEventListener('keyup', function(){
-  clearButton.disabled = userInput.value === '';
-})
-
 guessButton.addEventListener('click', function() {
   showLastGuess.innerText = userInput.value;
   resetButton.disabled = userInput.value === '';
-  var intGuess = userInput.value;
-    if(isNaN(userInput.value)) {
-      alert("Not a number")
-    } else if (intGuess == secretNumber) {
+  var intGuess = parseInt(userInput.value);
+    if(isNaN(intGuess)) {
+      alert("Please enter a number.")
+    } else if (intGuess === secretNumber) {
       hint.innerText = "BOOM!";
     } else if (intGuess < 1 || intGuess > 100){
       alert("Please enter a number between 1 and 100");
     } else if (intGuess > secretNumber) {
       hint.innerText = "That is too high";
-    } else if (intGuess < secretNumber) {
+    } if (intGuess < secretNumber) {
       hint.innerText = "That is too low";
-    } else {
-      hint.innerText = "Please guess a number";
     }
 });
 
-resetButton.addEventListener('click', function () {
-  secretNumber = resetFunction();
-  clearButton.disabled = true;
-  resetButton.disabled = true;
-  //resetButton.disabled = true;
-});
 
 function resetFunction () {
   showLastGuess.innerText = "";
   hint.innerText = "Please guess a number";
   userInput.value = null;
-  var newSecret = Math.floor(Math.random() * 101);
+  var newSecret = Math.floor(Math.random() * 101 + 1);
   console.log(newSecret);
   return newSecret;
 };
